803e01be47e5fcf4ca987fd54a24d3ce0d1a4023
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-06-20 19:28:09 +0530

    Added Getting Started sample for SfNumericEntry

---
+++ NumericEntryGettingStarted/NumericEntryGettingStarted/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NumericEntryGettingStarted;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NumericEntryGettingStarted/NumericEntryGettingStarted/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NumericEntryGettingStarted;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
